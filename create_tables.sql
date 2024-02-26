708522ebf8c1b4bf8279792b45538e8770cfe177
AlexDev404 <alexdev404@users.noreply.github.com>
2024-02-26 01:36:52 -0600

    Finally, create the SQL create_table whatever thing

---
+++ create_tables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userName VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    sk VARCHAR(255) NOT NULL,
+    addr VARCHAR(255) NOT NULL
+    role TINYINT(1) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    c_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT NOT NULL,
+    rnt_status TINYINT(1) NOT NULL,
+    type TINYINT(1) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    rating DECIMAL(2, 1) NOT NULL,
+    FOREIGN KEY (c_id) REFERENCES categories(id),
+);
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    desc TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS rentals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product INT NOT NULL,
+    returned_at BIGINT NOT NULL,
+    rented_at BIGINT NOT NULL,
+    is_due_at BIGINT NOT NULL,
+    charge DECIMAL(10, 2) NOT NULL,
+    user INT NOT NULL,
+    FOREIGN KEY (product) REFERENCES products(id),
+    FOREIGN KEY (user) REFERENCES users(id)
+);
